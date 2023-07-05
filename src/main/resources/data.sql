cd1242f82da941d8c6890f307396e3948d6f31b4
jovana112 <jovanamilisic1000@yahoo.com>
2023-07-05 19:01:29 +0200

    apr test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -53,12 +53,25 @@ INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('1234123
 INSERT INTO service_role(id, attributes, role, service) VALUES (10, '{"id":1}', "ROLE_USER", "szz");
 INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('123412341233',10);
 
-INSERT INTO service_role(id, attributes, role, service) VALUES (11, '{"companyId":null}', "ROLE_USER", "apr");
-INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('123412341234', 11);
+
+INSERT INTO user(username, father_jmbg, first_name, last_name, mother_jmbg, password)
+VALUES ('12345', NULL, 'Sima', 'Simic', NULL, '$2a$12$abC/Iqm56FM/G4oOZ.xXs.KEuhE5RtQ0oyHQxJngaC1cPUq/S/i.e');
+
+INSERT INTO service_role(id, attributes, role, service) VALUES (11, '{"schoolId":2}', "ROLE_STUDENT", "school");
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('12345', 11);
 
 
-INSERT INTO service_role(id, attributes, role, service) VALUES (12, '{"companyId":2}', "ROLE_ADMIN", "apr");
-INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('1234567890123', 12);
+INSERT INTO service_role(id, attributes, role, service) VALUES (12, '{"id":2}', "ROLE_USER", "szz");
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('12345',12);
 
 INSERT INTO service_role(id, attributes, role, service) VALUES (13, '{"companyId":null}', "ROLE_USER", "apr");
-INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('123412341233', 13);
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('123412341234', 13);
+
+INSERT INTO service_role(id, attributes, role, service) VALUES (14, '{"companyId":2}', "ROLE_USER", "apr");
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('1234567890123', 14);
+
+INSERT INTO service_role(id, attributes, role, service) VALUES (15, '{"companyId":null}', "ROLE_USER", "apr");
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('123412341233', 15);
+
+INSERT INTO service_role(id, attributes, role, service) VALUES (16, '{"companyId":null}', "ROLE_USER", "apr");
+INSERT INTO user_service_roles(user_username, service_roles_id) VALUES ('12345', 16);
